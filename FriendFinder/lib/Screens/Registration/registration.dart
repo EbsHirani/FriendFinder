bcfb68053c790b07473178b3cd1375ac442c1c54
EbsHirani <hiraniebrahim13@gmail.com>
2020-10-26 17:43:57 +0530

    p

--- FriendFinder/lib/Screens/Registration/registration.dart
+++ FriendFinder/lib/Screens/Registration/registration.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:date_field/date_field.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:friendfinder/components/rounded_button.dart';
 import 'package:friendfinder/components/rounded_input_field.dart';
 import 'package:friendfinder/constants.dart';
@@ -23,10 +25,11 @@ class Registration extends StatefulWidget {
 }
 
 class _RegistrationState extends State<Registration> {
-  String bio;
+  String bio, lang;
   File file;
   String uid, url, name, email;
   StorageReference ref;
+  DateTime selectedDate;
   // File img;
   File _image;
   List<bool> bools = [];
@@ -37,6 +40,12 @@ class _RegistrationState extends State<Registration> {
   // StorageUploadTask _uploadTask;
 
   List all = ["Music", "fadalsjlfs", "asdhfaskj"];
+  List<String> languages = [
+    "English",
+    "Hindi",
+    "Gujarati",
+    "Marathi",
+  ];
 
   List selected = [];
 
@@ -76,7 +85,7 @@ class _RegistrationState extends State<Registration> {
   // }
   Future<bool> getUser(String uid) async{
     http.Response res = await http.post(
-                  'http://10.0.2.2:5000/register',
+                  'http://10.0.2.2:5000/get_user_profile',
                   headers: <String, String>{
                     'Content-Type': 'application/json; charset=UTF-8',
                   },
@@ -87,10 +96,12 @@ class _RegistrationState extends State<Registration> {
     Map map = jsonDecode(res.body);
     name = map["name"];
     try{
-
+    url = map["profile_picture"];
     bio = map["bio"];
     email = map["email"];
     selected = map["interest"];
+    lang = map["languages"];
+    selectedDate = map["dob"];
     }
     catch (e){
       print(e);
@@ -117,6 +128,7 @@ class _RegistrationState extends State<Registration> {
           FirebaseStorage.instance.ref().child(fileName);
       StorageUploadTask uploadTask = firebaseStorageRef.putFile(_image);
       StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
+      url = (await firebaseStorageRef.getDownloadURL()).toString();
       setState(() {
         print("Profile Picture uploaded");
         Scaffold.of(context)
@@ -190,9 +202,9 @@ class _RegistrationState extends State<Registration> {
                   ),
                 ),
                 SizedBox(height: kSpacingUnit.w * 2),
-                Text('Adam', style: kTitleTextStyle),
+                Text(name, style: kTitleTextStyle),
                 SizedBox(height: kSpacingUnit.w * 0.5),
-                Text('Adam@gmail.com', style: kCaptionTextStyle),
+                Text(email, style: kCaptionTextStyle),
                 SizedBox(height: kSpacingUnit.w * 2),
                 Column(children: [
                   Container(
@@ -203,6 +215,52 @@ class _RegistrationState extends State<Registration> {
                         bio = value;
                       },
                     ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: DateTimeField(
+                selectedDate: selectedDate,
+                onDateSelected: (DateTime date) {
+                    setState(() {
+                      selectedDate = date;
+                    });
+                },
+                lastDate: DateTime(2020),
+              ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: DropdownButtonFormField(
+              isExpanded: true,
+                items: languages.map((String category) {
+                    return new DropdownMenuItem(
+                        value: category,
+                        child: Row(                               
+                          children: <Widget>[
+                            Icon(Icons.arrow_right),
+                            Flexible(
+                              child: Text(category),
+                            ),
+                          ],
+                          
+                        )
+                      );
+                }).toList(),
+                onChanged: (newValue) async {
+                    // do other stuff with _category
+                    setState(() => lang = newValue);
+                // await db.setCurrentstream(newValue);
+                },
+                value: lang,
+                decoration: InputDecoration(
+                    enabledBorder:
+                        UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+                    contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    filled: true,
+                    // fillColor: Theme.of(context).primaryColor,
+                    errorStyle: TextStyle(color: Colors.redAccent, fontSize: 16.0),
+                    hintText: 'Choose Prefered Language',
+                )),
                   ),
                   Align(
                     child: GridView.builder(
@@ -274,7 +332,36 @@ class _RegistrationState extends State<Registration> {
                 Spacer(),
                 RoundedButton(
                   text: "Confirm",
-                  press: () {},
+                  press: () async {
+                    if(name == null || lang == null || bio == null || selectedDate ==null || selected.length == 0){
+                      Fluttertoast.showToast(
+                      msg: "Enter all the fields",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.CENTER,
+                      timeInSecForIosWeb: 1,
+                      backgroundColor: Colors.red,
+                      textColor: Colors.white,
+                      fontSize: 16.0
+                  );
+                    }
+                    else 
+                      {http.Response res = await http.post(
+                    'http://10.0.2.2:5000/set_user_profile',
+                    headers: <String, String>{
+                      'Content-Type': 'application/json; charset=UTF-8',
+                    },
+                    body: jsonEncode(<String, dynamic>{
+                      "name" : name,
+                      "bio" : bio,
+                      "interest" : selected,
+                      "user_id": uid,
+                      "profile_picture" : url,
+                      "languages": lang,
+                    }),
+                  );
+                    Map map = jsonDecode(res.body);
+                    }
+                  },
                 )
               ],
             ),
