6e13951a8b2b0a853b0813a7933c73eea5947f03
EbsHirani <hiraniebrahim13@gmail.com>
2020-10-26 17:08:08 +0530

    reg

--- FriendFinder/lib/Screens/Registration/registration.dart
+++ FriendFinder/lib/Screens/Registration/registration.dart
@@ -19,7 +19,7 @@ class Registration extends StatefulWidget {
     this.uid
   });
   @override
-  _RegistrationState createState() => _RegistrationState();
+  _RegistrationState createState() => _RegistrationState(uid : uid);
 }
 
 class _RegistrationState extends State<Registration> {
@@ -30,14 +30,19 @@ class _RegistrationState extends State<Registration> {
   // File img;
   File _image;
   List<bool> bools = [];
-  final FirebaseStorage _storage =
-      FirebaseStorage(storageBucket: 'gs://friend-finder-69982.appspot.com/');
 
-  StorageUploadTask _uploadTask;
+  // final FirebaseStorage _storage =
+  //     FirebaseStorage(storageBucket: 'gs://friend-finder-69982.appspot.com/');
+
+  // StorageUploadTask _uploadTask;
 
   List all = ["Music", "fadalsjlfs", "asdhfaskj"];
 
   List selected = [];
+
+  _RegistrationState({
+    this.uid
+  });
 
   // void _startUpload() async{
 
@@ -90,7 +95,7 @@ class _RegistrationState extends State<Registration> {
     catch (e){
       print(e);
     }
-
+    return true;
 
   }
   @override
@@ -130,144 +135,155 @@ class _RegistrationState extends State<Registration> {
     }
 
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              height: kSpacingUnit.w * 10,
-              width: kSpacingUnit.w * 10,
-              margin: EdgeInsets.only(top: kSpacingUnit.w * 3),
-              child: Container(
-                height: kSpacingUnit.w * 10,
-                width: kSpacingUnit.w * 10,
-                margin: EdgeInsets.only(top: kSpacingUnit.w * 3),
-                child: Stack(
-                  children: [
-                    CircleAvatar(
-                      radius: 100,
-                      // radius: kSpacingUnit.w * 5,
-                      child: SizedBox(
-                          width: 180,
-                          height: 180,
-                          child: _image == null
-                              ? Image.asset('assets/images/avatar.png',
-                                  fit: BoxFit.fill)
-                              : Image.file(_image, fit: BoxFit.fill)),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: InkWell(
-                        onTap: () async {
-                          getImage();
-                        },
-                        child: Container(
-                          height: kSpacingUnit.w * 2.5,
-                          width: kSpacingUnit.w * 2.5,
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).accentColor,
-                            shape: BoxShape.circle,
-                          ),
-                          child: Icon(
-                            LineAwesomeIcons.pen,
-                            color: kDarkPrimaryColor,
-                            size: ScreenUtil().setSp(kSpacingUnit.w * 1.5),
-                          ),
+      body: FutureBuilder(
+        future: getUser(uid),
+        builder: (context, snapshot) {
+          if(snapshot.hasData){
+
+          return Container(
+            child: Column(
+              children: [
+                Container(
+                  height: kSpacingUnit.w * 10,
+                  width: kSpacingUnit.w * 10,
+                  margin: EdgeInsets.only(top: kSpacingUnit.w * 3),
+                  child: Container(
+                    height: kSpacingUnit.w * 10,
+                    width: kSpacingUnit.w * 10,
+                    margin: EdgeInsets.only(top: kSpacingUnit.w * 3),
+                    child: Stack(
+                      children: [
+                        CircleAvatar(
+                          radius: 100,
+                          // radius: kSpacingUnit.w * 5,
+                          child: SizedBox(
+                              width: 180,
+                              height: 180,
+                              child: _image == null
+                                  ? Image.asset('assets/images/avatar.png',
+                                      fit: BoxFit.fill)
+                                  : Image.file(_image, fit: BoxFit.fill)),
                         ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: kSpacingUnit.w * 2),
-            Text('Adam', style: kTitleTextStyle),
-            SizedBox(height: kSpacingUnit.w * 0.5),
-            Text('Adam@gmail.com', style: kCaptionTextStyle),
-            SizedBox(height: kSpacingUnit.w * 2),
-            Column(children: [
-              Container(
-                padding: EdgeInsets.all(10),
-                child: RoundedInputField(
-                  hintText: "Your Bio",
-                  onChanged: (value) {
-                    bio = value;
-                  },
-                ),
-              ),
-              Align(
-                child: GridView.builder(
-                  shrinkWrap: true,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    childAspectRatio: 5,
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 10.0,
-                    mainAxisSpacing: 10.0,
-                  ),
-                  itemCount: all.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                        child: Container(
-                          child: CheckboxListTile(
-                              controlAffinity: ListTileControlAffinity.leading,
-                              activeColor: Colors.white,
-                              checkColor: Colors.yellow,
-                              title: Text(
-                                "${all[index]}",
-                                style: TextStyle(
-                                    color: selected.contains(all[index])
-                                        ? Colors.yellow
-                                        : Colors.white),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: InkWell(
+                            onTap: () async {
+                              getImage();
+                            },
+                            child: Container(
+                              height: kSpacingUnit.w * 2.5,
+                              width: kSpacingUnit.w * 2.5,
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).accentColor,
+                                shape: BoxShape.circle,
                               ),
-                              value: selected.contains(all[index]),
-                              onChanged: (val) {
-                                String checkboxText = all[index];
-
-                                if (val == true) {
-                                  setState(() {
-                                    selected.add(checkboxText);
-                                    // isLoading = true;
-                                  });
-                                  // await pr.show();
-                                  // DatabaseService(uid: widget.uid).addPassion(checkboxText);
-                                  //                                filteredPassions.remove(checkboxText);
-                                  //                                filteredPassions.insert(0, checkboxText);
-                                  // setState(() {
-                                  //   isLoading = false;
-                                  // });
-                                } else {
-                                  print("remove");
-
-                                  setState(() {
-                                    selected.remove(checkboxText);
-                                    // isLoading = true;
-                                  });
-                                  // await pr.show();
-                                  //  DatabaseService(uid: widget.uid).deletePassion(checkboxText);
-                                  //  setState(() {
-                                  //    isLoading = false;
-                                  //  });
-                                  // }
-                                  print(selected);
-                                  //  await pr.hide();
-                                }
-                              }),
-                        ));
-                  },
+                              child: Icon(
+                                LineAwesomeIcons.pen,
+                                color: kDarkPrimaryColor,
+                                size: ScreenUtil().setSp(kSpacingUnit.w * 1.5),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-            ]),
-            Spacer(),
-            RoundedButton(
-              text: "Confirm",
-              press: () {},
-            )
-          ],
-        ),
+                SizedBox(height: kSpacingUnit.w * 2),
+                Text('Adam', style: kTitleTextStyle),
+                SizedBox(height: kSpacingUnit.w * 0.5),
+                Text('Adam@gmail.com', style: kCaptionTextStyle),
+                SizedBox(height: kSpacingUnit.w * 2),
+                Column(children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: RoundedInputField(
+                      hintText: "Your Bio",
+                      onChanged: (value) {
+                        bio = value;
+                      },
+                    ),
+                  ),
+                  Align(
+                    child: GridView.builder(
+                      shrinkWrap: true,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        childAspectRatio: 5,
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10.0,
+                        mainAxisSpacing: 10.0,
+                      ),
+                      itemCount: all.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            margin:
+                                EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                            child: Container(
+                              child: CheckboxListTile(
+                                  controlAffinity: ListTileControlAffinity.leading,
+                                  activeColor: Colors.white,
+                                  checkColor: Colors.yellow,
+                                  title: Text(
+                                    "${all[index]}",
+                                    style: TextStyle(
+                                        color: selected.contains(all[index])
+                                            ? Colors.yellow
+                                            : Colors.white),
+                                  ),
+                                  value: selected.contains(all[index]),
+                                  onChanged: (val) {
+                                    String checkboxText = all[index];
+
+                                    if (val == true) {
+                                      setState(() {
+                                        selected.add(checkboxText);
+                                        // isLoading = true;
+                                      });
+                                      // await pr.show();
+                                      // DatabaseService(uid: widget.uid).addPassion(checkboxText);
+                                      //                                filteredPassions.remove(checkboxText);
+                                      //                                filteredPassions.insert(0, checkboxText);
+                                      // setState(() {
+                                      //   isLoading = false;
+                                      // });
+                                    } else {
+                                      print("remove");
+
+                                      setState(() {
+                                        selected.remove(checkboxText);
+                                        // isLoading = true;
+                                      });
+                                      // await pr.show();
+                                      //  DatabaseService(uid: widget.uid).deletePassion(checkboxText);
+                                      //  setState(() {
+                                      //    isLoading = false;
+                                      //  });
+                                      // }
+                                      print(selected);
+                                      //  await pr.hide();
+                                    }
+                                  }),
+                            ));
+                      },
+                    ),
+                  ),
+                ]),
+                Spacer(),
+                RoundedButton(
+                  text: "Confirm",
+                  press: () {},
+                )
+              ],
+            ),
+          );
+          }
+          else{
+            return Center(child: CircularProgressIndicator(),);
+          }
+        }
       ),
     );
   }
